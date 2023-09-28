80c1a35a64a0af91a01d781ed61b58cc745229e1
dntrieunguyen <dntrieunguyen@gmail.com>
2023-09-28 19:25:32 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import './App.css';
+import './App.scss';
 
 function App() {
-   return <div></div>;
+   return (
+      <div>
+         <h2 className="text-title text-success font-bold">title</h2>
+
+         <h2 className="text-info text-error font-bold">info</h2>
+
+         <p className="text-content text-secondary font-bold">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate
+            voluptatum, vero labore at odio tempore nostrum repellendus nisi
+            repellat vel!
+         </p>
+      </div>
+   );
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,25 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
    mode: 'jit',
    content: ['./src/**/*.{js,jsx,ts,tsx}'],
    theme: {
-      extend: {},
+      extend: {
+         colors: {
+            default: '#020617',
+            primary: '#f8fafc',
+            secondary: '#015C92',
+            success: '#16a34a',
+            warning: '#FE7A15',
+            error: '#dc2626',
+         },
+
+         fontSize: {
+            content: '1rem',
+            info: '1.5rem',
+            title: '3rem',
+         },
+      },
    },
    plugins: [],
 };
